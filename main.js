2afbdab1fe4c9d0a053f840053ba3cd795dafba9
Smith Thay <S.T.@Kathryns-MacBook-Pro.local>
2021-02-12 10:36:47 -0500

    added banner and minor changes

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function result() {
     }
 
     if(document.getElementById("uk").checked) {
-    let weight = Math.round(300/14) + 'stone';
+    let weight = Math.round(300/14) + ' stone';
     let temperature =  Math.round((94 - 32) * 5 / 9) + ' cetigrade';
     
     newStory = newStory.replace('94 fahrenheit',temperature);
